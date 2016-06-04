c3f2e1c557c48001bf15110b1ba2c8019c8840bc
matthewstifler <mtwstflr@gmail.com>
2016-06-04 22:07:17 +0300

    numeric vars cleanup

--- code/script.R
+++ code/script.R
@@ -25,4 +25,18 @@ sec.guard = ifelse((str_count(observation.data$security, "u'guards'") > 0) | (st
 sec.fence = ifelse((str_count(observation.data$security, "u'fence'") > 0) | (str_count(observation.data$security, "2") > 0) | (str_count(observation.data$security, "3") > 0), 1, 0)
 sec.cctv = ifelse((str_count(observation.data$security, "u'surveillance_facade'") > 0) | (str_count(observation.data$security, "4") > 0), 1, 0)
 
-observation.data = cbind(observation.data, as.factor(sec.no), sec.guard %>% as.factor, sec.fence %>% as.factor, sec.cctv %>% as.factor)
+observation.data = cbind(observation.data, sec.no = as.factor(sec.no), sec.guard = sec.guard %>% as.factor, sec.fence = sec.fence %>% as.factor, sec.cctv = sec.cctv %>% as.factor)
+
+#numeric vars
+levels(observation.data$windows.broken.share)[c(1:2, 59, 65:81, 84:85, 88:89)] = NA
+levels(observation.data$windows.broken.share)[c(21, 35:37, 43, 61, 64:65)] = NA
+levels(observation.data$windows.broken.share)[c(2,8,11,15,18)] = c(0, 0.05, 0.1, 0.1,0.2)
+levels(observation.data$windows.broken.share)[c(22:52)] = c(NA, 1, 0.1, 1, NA, NA, 0.15, NA, NA, 0.2, NA, NA, NA, 0.03, 0.33, NA, 0.4, NA, 0.05, NA, NA, 0.6, NA, 0.07, NA, NA, 0.9, 1, 1, 0.25, 0)
+levels(observation.data$windows.broken.share)[c(17,19)] = c(0.5, 0.9)
+observation.data$windows.broken.share %<>% as.character %>% as.numeric
+
+levels(observation.data$windows.bars.share) = levels(observation.data$windows.bars.share) %>% str_replace_all(",", ".")
+levels(observation.data$windows.bars.share)[c(1:7, 15, 31, 38:41, 46, 50, 60, 62, 68, 70:77, 78:88)] = c(NA, NA, rep(0,5), 0.05, 0.14, rep(NA, 4), NA, 0.2, 0.5, NA, NA, NA, 1, 0.1, 1, 1, 0.1, NA, 0.5, rep(NA, 6), 0.15, NA, NA, 0.33)
+levels(observation.data$windows.bars.share)[c(53:62, 64, 66, 68:70, 72:79, 81:84, 86:96, 99, 102, 103, 105:107, 111, 114:126, 128:153, 155: 163, 166:170)] = NA
+levels(observation.data$windows.bars.share)[c(53:72)] = c(0.2, 1, 0.22, 0.25, 0.3, 0.33, 0.05, 0.05, 0.5, 0.5, 0.6, 0.8, 0.5, 1, 0.9, 0.95, 1, 0, 0.017, 0.063)
+observation.data$windows.bars.share %<>% as.character %>% as.numeric
